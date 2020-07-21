f877791ab02a4fbb8825805276e939fdc3474751
Dragon <dragon.t.tran+code@gmail.com>
2020-07-21 17:49:31 -0500

    Updates access specifiers

--- Sources/TwitterSwiftLite/TwitterSwiftLite.swift
+++ Sources/TwitterSwiftLite/TwitterSwiftLite.swift
@@ -1,7 +1,7 @@
 import Foundation
 import FoundationNetworking
 
-struct TwitterSignatureParameters {
+public struct TwitterSignatureParameters {
   let includeEntities: Bool
   let oauthConsumerKey: String
   let oauthSignatureMethod: String
